10dab422353de69da53c366d9a2d2727e76efc69
palukiou <palukiou@monordi.monordi>
2025-12-17 14:28:55 +0100

    test

--- ex00/includes/Animal.hpp
+++ ex00/includes/Animal.hpp
@@ -13,7 +13,7 @@ class Animal {
 		Animal(const Animal &animal);
 		Animal& operator=(const Animal &animal);
 		virtual void	makeSound() const; // specifying const tells that the method cannot change the object state.
-		virtual ~Animal();
+		virtual ~Animal(); // destructor should be virtual otherwise the child destructor will never be called if the object is created from Animal *b = new Cat();
 		std::string getType() const;
 };
 
